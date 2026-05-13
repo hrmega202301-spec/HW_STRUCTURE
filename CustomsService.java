cc66b568ffd0d552328905d226e514811ddc4141
hrmega202301-spec <hrmega202301@gmail.com>
2026-05-13 21:15:40 +0300

    Delete CustomsService.java

--- CustomsService.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.service;
-
-public class CustomsService{
-    public static final int CALCULATE_CUSTOMS(int price, int weight) {
-        int result = (price / 100) + (weight * 100);
-        return result;
-    }
-}
