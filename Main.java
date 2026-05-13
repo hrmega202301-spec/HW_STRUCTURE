1a2fb3096182eae4633ab77403f8dfe0429355a6
hrmega202301-spec <hrmega202301@gmail.com>
2026-05-13 20:55:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+import ru.netology.service.CustomsService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите цену товара (в руб.):");
+        int price = scanner.nextInt();
+        System.out.println("Введите вес товара (в кг.):");
+        int weight = scanner.nextInt();
+        System.out.println("Размер пошлины (в руб.) составит: " + CustomsService.CALCULATE_CUSTOMS(price, weight));
+    }
+}
